bba94c283cf964da9dfbc53d873f938d582f08eb
Bradley Goldmeier <bradleygoldmeier@yahoo.com>
2018-11-21 19:22:08 -0500

    first main, clock and alarm outline

--- src/com/company/Alarm.java
+++ src/com/company/Alarm.java
@@ -1,7 +1,14 @@
 package com.company;
 
+import java.time.LocalTime;
+
 /**
  * Created by Bradley Goldmeier on 11/19/2018.
  */
 public class Alarm {
+    public void setAlarm() {
+    }
+
+    public static void checkAlarm(LocalTime now) {
+    }
 }
--- src/com/company/Clock.java
+++ src/com/company/Clock.java
@@ -1,16 +1,25 @@
 package com.company;
 
-import java.text.SimpleDateFormat;
-import java.time.DateTimeException;
 import java.time.LocalDateTime;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalTime;
 
 /**
  * Created by Bradley Goldmeier on 11/19/2018.
  */
 public class Clock {
-    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-    System.out.println("hero");
 
+    public void updateAndPrintTime() {
+        LocalTime now = LocalTime.now();
+        if (now.getNano() == 0) {
+            System.out.println(now);
+        }
+
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Alarm.checkAlarm(now);
+    }
 }
