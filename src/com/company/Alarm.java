fab7778ca9e6afcc2776d27cc85659288907f141
Unknown <jordannpenn@gmail.com>
2018-11-24 20:13:06 -0500

    First edition alarm class

--- src/com/company/Alarm.java
+++ src/com/company/Alarm.java
@@ -1,14 +1,42 @@
 package com.company;
 
+import sun.awt.image.ImageWatched;
+
 import java.time.LocalTime;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 /**
- * Created by Bradley Goldmeier on 11/19/2018.
+ * Created by Nachi Penn on 11/23/2018.
  */
 public class Alarm {
+    public static LinkedList<LocalTime> alarms = new LinkedList();
+    Scanner scanner = new Scanner(System.in);
+
     public void setAlarm() {
+        int hour;
+        int minute;
+
+        System.out.println("what hour would you like your alarm to ring (0-23)?");
+        hour = scanner.nextInt();
+
+        System.out.println("what minute would you like your alarm to ring (0-59)?");
+        minute = scanner.nextInt();
+
+        System.out.printf("you set an alarm at %02d:%02d", hour, minute);
+        System.out.println();
+
+        alarms.add(LocalTime.of(hour,minute));
+        //addAlarm(hour, minute);
     }
 
     public static void checkAlarm(LocalTime now) {
+        for(int i = 0; i < alarms.size(); i++) {
+            if (now.getHour() == alarms.peek().getHour() && now.getMinute() == alarms.peek().getMinute()){
+                System.out.println("BEEP!!!!!");
+            }
+        }
     }
 }
